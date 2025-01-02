24623100338b5ed924e3470e0412569f55f65ada
Evan Bancroft <evan@riffcreativestudio.com>
2025-01-01 18:15:40 -0800

    Add Snacks.nvim, Prune Aerospace Config, Fix Ghostty Cursor

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,5 @@
 return {
 	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {
-			transparent = true,
-			style = "storm",
-			dim_inactive = true, -- dims inactive windows
-			lualine_bold = true,
-		},
-	},
-	{
 		"rose-pine/neovim",
 		name = "rose-pine",
 		lazy = false,
--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -6,18 +6,6 @@ return {
 		end,
 	},
 	{
-		"kdheepak/lazygit.nvim",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			require("telescope").load_extension("lazygit")
-			-- vim.keymap.set("n", "<leader>lg", "<cmd>Telescope lazygit<cr>")
-			vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>")
-		end,
-	},
-	{
 		"sindrets/diffview.nvim",
 		config = function()
 			vim.keymap.set("n", "<leader>dv", "<cmd>DiffviewOpen<cr>")
