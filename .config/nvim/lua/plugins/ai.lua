a456e3f9a661f67efd5976f09630869544bdf3d2
EvanBancroft <evan.bancroft@soundstripe.com>
2025-05-01 12:00:23 -0700

    Add CodeCompanion

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,5 +1,54 @@
 return {
 	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			adapters = {
+				openai = function()
+					return require("codecompanion.adapters").extend("openai", {
+						schema = {
+							model = {
+								default = "claude-3-7-sonnet-20250219",
+							},
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = {
+					adapter = "anthropic",
+				},
+				inline = {
+					adapter = "anthropic",
+				},
+			},
+			display = {
+				chat = {
+					window = {
+						position = "right",
+					},
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>cm",
+				"<cmd>CodeCompanionActions<cr>",
+				desc = "Open [C]ode Companion [M]enu",
+				mode = { "v", "n" },
+			},
+			{
+				"<leader>cc",
+				"<cmd>CodeCompanionChat Toggle<cr>",
+				desc = "Open [C]ode Companion [C]hat",
+				mode = { "v", "n" },
+			},
+		},
+	},
+	{
 		"supermaven-inc/supermaven-nvim",
 		keys = {
 			{ "<leader>ai", "<cmd>SupermavenToggle<cr>", desc = "Supermaven Toggle" },
